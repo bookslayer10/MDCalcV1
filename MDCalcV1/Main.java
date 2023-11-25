d840c45b65e1138694a8758ed19d238546d35959
bookslayer10 <93895819+bookslayer10@users.noreply.github.com>
2023-11-24 16:00:13 -0800

    Made project folder

---
+++ MDCalcV1/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Title: Mount Douglas Calculator Version 1
+ * Authors: Liam Teschke,
+ * 
+ * Date: Nov 24th, 2023
+ * Purpose: 
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
